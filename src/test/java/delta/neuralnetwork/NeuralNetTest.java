2db5ebab9ab4c0e5448c7809e4c7fd04af8070a8
asanchez89 <idrian89@gmail.com>
2024-01-14 17:13:10 -0400

    Implementación sumar columnas

--- src/test/java/delta/neuralnetwork/NeuralNetTest.java
+++ src/test/java/delta/neuralnetwork/NeuralNetTest.java
@@ -10,7 +10,20 @@ import delta.matrix.Matrix;
 
 public class NeuralNetTest {
 	private Random random = new Random();
-	
+
+	@Test
+	public void testSumColumns() {
+		Matrix m = new Matrix(4, 5, i -> i);
+
+		Matrix result = m.sumColumns();
+
+		double[] expectedValues = { +30.00000, +34.00000, +38.00000, +42.00000, +46.00000 };
+		Matrix expected = new Matrix(1, 5, i->expectedValues[i]);
+		
+		assertTrue(expected.equals(result));
+
+	}
+
 	@Test
 	public void testAddBias() {
 		Matrix input = new Matrix(3, 3, i -> (i + 1));
@@ -21,35 +34,35 @@ public class NeuralNetTest {
 
 		double[] expectedValues = { +31.00000, +37.00000, +43.00000, +68.00000, +83.00000, +98.00000, +105.00000,
 				+129.00000, +153.00000 };
-		Matrix expected = new Matrix(3,3, i->expectedValues[i]);
-		
+		Matrix expected = new Matrix(3, 3, i -> expectedValues[i]);
+
 		assertTrue(expected.equals(result));
-		
+
 	}
 
 	@Test
 	public void testReLu() {
-		
+
 		final int numberNeurons = 5;
 		final int numberInputs = 6;
 		final int inputSize = 4;
-		
+
 		Matrix input = new Matrix(inputSize, numberInputs, i -> random.nextDouble());
 		Matrix weights = new Matrix(numberNeurons, inputSize, i -> random.nextGaussian());
 		Matrix biases = new Matrix(numberNeurons, 1, i -> random.nextGaussian());
 
 		Matrix result1 = weights.multiply(input).modify((row, col, value) -> value + biases.get(row));
-		Matrix result2 = weights.multiply(input).modify((row, col, value) -> value + biases.get(row)).modify(value->value>0?value:0);
-		
-		result2.forEach((index, value)->{
+		Matrix result2 = weights.multiply(input).modify((row, col, value) -> value + biases.get(row))
+				.modify(value -> value > 0 ? value : 0);
+
+		result2.forEach((index, value) -> {
 			double originalValue = result1.get(index);
-			if(originalValue > 0) {
-				assertTrue(Math.abs(originalValue - value)<0.000001);
-			}else {
-				assertTrue(Math.abs(value)<0.000001);
+			if (originalValue > 0) {
+				assertTrue(Math.abs(originalValue - value) < 0.000001);
+			} else {
+				assertTrue(Math.abs(value) < 0.000001);
 			}
 		});
 
-		
 	}
 }
